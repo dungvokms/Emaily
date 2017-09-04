e378b0c3ea61326b2f09bcc325ab9bd1c41364cc
Dung Vo <dungvo@kms-technology.com>
2017-09-04 17:31:24 +0700

    Update code

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,21 @@
 const passport = require('passport');
 
-module.exports = (app) => {
-  app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-  }));
+module.exports = app => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/users/me/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get('/api/users/me', (req, res) => {
+    res.send(req.user);
+  });
 };
